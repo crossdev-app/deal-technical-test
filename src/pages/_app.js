ba466d6775283edca61ece84300c0f7cf601705d
crossdev <malikdqa@gmail.com>
2023-01-18 20:32:11 +0700

    Fix font issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,19 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from "react-redux";
 import "src/styles/custom_style/custom_style.css";
 import store from "../store/store";
+import { Nunito_Sans } from "@next/font/google";
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  weight: ["200", "400", "300", "600", "700", "800", "900"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <div className={nunito.className}>
+        <Component {...pageProps} />
+      </div>
     </Provider>
   );
 }
